0ddd16711d76810b192543d5f2977d09407a6997
RAJU-19390 <www.rajuboda333@gmail.com>
2024-02-02 13:37:44 +0530

    Add project files.

---
+++ CricketPlayer/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlayerDetails;
+
+namespace CricketPlayer
+{
+    class Program
+    {
+        static CricketTeam cricket_team = new CricketTeam();
+        static void Main(string[] args)
+        {
+
+            char ch='Y';
+            do {
+
+                Console.WriteLine("1.Add Player Details(Id,Name,Age)\n2.Remove Player from Team by entering Id\n3.Display player details by entering Id\n4.Display player Details by entering name\n5.Display All Player Details\n6.Exit\nSelect One Option : ");
+                int userchoice = Convert.ToInt32(Console.ReadLine());
+                switch (userchoice)
+                {
+                    case 1:
+                        AddPlayerDetails();
+                        break;
+                    case 2:
+                        RemovePlayerById();
+                        break;
+                    case 3:
+                        PlayerDetailsById();
+                        break;
+                    case 4:
+                        PlayerDetailsByName();
+                        break;
+                    case 5:
+                        AllPlayerDetails();
+                        break;
+                    case 6:
+                        Environment.Exit(1);
+                        break;
+                    default: Console.WriteLine("Oops!Invalid option!!Enter correct option...");
+                        break;
+                }
+                Console.WriteLine("***********************************************************************************");
+
+            } while (ch=='Y'||ch=='y');
+            Console.ReadLine();
+        }
+        static void AddPlayerDetails()
+        {
+            cricket_team.AddPlayer();
+        }
+
+        static void RemovePlayerById()
+        {
+            Console.WriteLine("Enter Player Id to remove");
+            int removeid = Convert.ToInt32(Console.ReadLine());
+            cricket_team.RemovePlayer(removeid);
+        }
+
+        static void PlayerDetailsById()
+        {
+            Console.WriteLine("Enter Player Id to display details : ");
+            int Id = Convert.ToInt32(Console.ReadLine());
+            Player playerdetailsbyid = cricket_team.GetPlayerById(Id);
+            if (playerdetailsbyid != null)
+            { 
+                Console.WriteLine($"PlayerId: {playerdetailsbyid.PlayerId} | PlayerName: {playerdetailsbyid.PlayerName} | PlayerAge: {playerdetailsbyid.PlayerAge}");
+            }
+            else
+            {
+                Console.WriteLine("Given ID of player not found");
+            }
+        }
+
+        static void PlayerDetailsByName()
+        {
+            Console.WriteLine("Enter Player Name to display details: ");
+            string playername = Console.ReadLine();
+            List<Player> playerdetailsbyname = cricket_team.GetPlayerByName(playername);
+            if (playerdetailsbyname.Count > 0)
+            {
+                foreach (var player in playerdetailsbyname)
+                {
+                    Console.WriteLine($"PlayerId: {player.PlayerId} | PlayerName: {player.PlayerName} | PlayerAge: {player.PlayerAge}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No players found with the given name.");
+            }
+        }
+        static void AllPlayerDetails()
+        {
+            if (cricket_team.players.Count > 0)
+            {
+                foreach (var player in cricket_team.GetAllPlayers())
+                {
+                    Console.WriteLine($"PlayerId: {player.PlayerId} | PlayerName: {player.PlayerName} | PlayerAge: {player.PlayerAge}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No player details found");
+            }
+        }
+    }
+}
+
---
+++ CricketPlayer/TeamPlayers.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlayerDetails;
+
+namespace CricketPlayer
+{
+
+    public interface ITeamPlayers
+    {
+        void AddPlayer();
+        void RemovePlayer(int playerId);
+        Player GetPlayerById(int playerId);
+        List<Player> GetPlayerByName(string playerName);
+        List<Player> GetAllPlayers();
+    }
+
+    public class CricketTeam : ITeamPlayers
+    {
+        public List<Player> players = new List<Player>();
+        
+
+        public void AddPlayer()
+        {
+            Console.WriteLine("Enter player details:");
+
+            while (players.Count <= 11)
+            {
+                Console.Write("Player Id: ");
+                int playerid = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Player Name: ");
+                string playername = Console.ReadLine();
+
+                Console.Write("Player Age: ");
+                int playerage = Convert.ToInt32(Console.ReadLine());
+
+                Player newplayer = new Player { PlayerId = playerid, PlayerName = playername, PlayerAge = playerage };
+
+                if (players.Count <= 11)
+                {
+                    players.Add(newplayer);
+                    Console.WriteLine($"{newplayer.PlayerName} added to Cricket Team.");
+                }
+                else
+                {
+                    Console.WriteLine("Team limit reached.Only 11 Players");
+                    break;
+                }
+                Console.Write("Do you want to add another player? (Y/N): ");
+                string userInput = Console.ReadLine().Trim();
+
+                if (userInput.ToUpper() != "Y")
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"{players.Count} Player(s) added to Cricket team successfully.");
+        }
+        public void RemovePlayer(int playerid)
+        {
+            Player PlayerToRemove = players.Find(p => p.PlayerId == playerid);
+            if (PlayerToRemove != null)
+            {
+                players.Remove(PlayerToRemove);
+                Console.WriteLine($"Player {PlayerToRemove.PlayerName} removed from the team.");
+            }
+            else
+            {
+                Console.WriteLine("Player not found in the team.");
+            }
+        }
+        public Player GetPlayerById(int playerid)
+        {
+            return players.Find(p => p.PlayerId == playerid);
+        }
+
+        public List<Player> GetPlayerByName(string playername)
+        {
+            return players.Where(p => p.PlayerName.Equals(playername, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        public List<Player> GetAllPlayers()
+        {
+            return players;
+        }
+
+    }
+}
